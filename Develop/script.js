8ccb6631686f2f51a1ab8dcd24b09e50df06ca8d
Brianna Hebeler <briannahebeler@gmail.com>
2020-10-28 16:49:56 -0400

    added array of all if,else if variables and commented it out bc my button is no longer working

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,7 @@ var numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var allChar = [symbolChar, numChar, lowerChar, upperChar];
+var selectChar = [selectAllChar, selectChar1, selectChar2, selectChar3, selectChar4, selectChar5, selectChar6, selectChar7, selectChar8, selectChar9, selectChar10, selectChar11, selectChar12, selectChar13, selectChar14];
 
 var passLength = passChar;
 
@@ -46,57 +47,6 @@ if (wantPass === true) {
     var passLowerChar = confirm("Click okay to confirm including lowercase characters.");
   }
 
-if (passSymbolChar === true && passNumChar === true && passUpperChar === true && passLowerChar === true) {
-  var selectAllChar = allChar;
-}
-else if (passSymbolChar === true && passNumChar === true && passUpperChar === true && passLowerChar === false) {
-  var selectChar1 = symbolChar + numChar + upperChar;
-}
-else if (passSymbolChar === true && passNumChar === true && passUpperChar === false && passLowerChar === false) {
-  var selectChar2 = symbolChar + numChar;
-}
-else if (passSymbolChar === true && passNumChar === false && passUpperChar === false && passLowerChar === false) {
-  var selectChar3 = symbolChar;
-}
-else if (passSymbolChar === true && passNumChar === true && passUpperChar === false && passLowerChar === true) {
-  var selectChar4 = symbolChar + numChar + lowerChar;
-}
-else if (passSymbolChar === true && passNumChar === false && passUpperChar === false && passLowerChar === true) {
-  var selectChar5 = symbolChar + lowerChar;
-}
-else if (passSymbolChar === false && passNumChar === false && passUpperChar === false && passLowerChar === true) {
-  var selectChar6 = lowerChar;
-}
-else if (passSymbolChar === false && passNumChar === true && passUpperChar === false && passLowerChar === false) {
-  var selectChar7 = numChar;
-}
-else if (passSymbolChar === false && passNumChar === false && passUpperChar === true && passLowerChar === false) {
-  var selectChar8 = upperChar;
-}
-else if (passSymbolChar === true && passNumChar === false && passUpperChar === true && passLowerChar === false) {
-  var selectChar9 = symbolChar + upperChar;
-}
-else if (passSymbolChar === true && passNumChar === false && passUpperChar === true && passLowerChar === true) {
-  var selectChar10 = symbolChar + upperChar + lowerChar;
-}
-else if (passSymbolChar === false && passNumChar === true && passUpperChar === true && passLowerChar === true) {
-  var selectChar11 = numChar + upperChar + lowerChar;
-}
-else if (passSymbolChar === false && passNumChar === true && passUpperChar === true && passLowerChar === false) {
-  var selectChar12 = numChar + upperChar;
-}
-else if (passSymbolChar === false && passNumChar === true && passUpperChar === false && passLowerChar === true) {
-  var selectChar13 = numChar + lowerChar;
-}
-else if (passSymbolChar === false && passNumChar === false && passUpperChar === true && passLowerChar === true) {
-  var selectChar14 = upperChar + lowerChar;
-}
-
-//   for (var i = 0; i < passLength; i++) {
-//   var pass = Math.floor(Math.random() + passLength)
-
-// }  
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -108,4 +58,56 @@ else if (passSymbolChar === false && passNumChar === false && passUpperChar === 
 generateBtn.addEventListener("click", writePassword);
 
 
+// if (passSymbolChar === true && passNumChar === true && passUpperChar === true && passLowerChar === true) {
+//   var selectAllChar = allChar;
+// }
+// else if (passSymbolChar === true && passNumChar === true && passUpperChar === true && passLowerChar === false) {
+//   var selectChar1 = symbolChar + numChar + upperChar;
+// }
+// else if (passSymbolChar === true && passNumChar === true && passUpperChar === false && passLowerChar === false) {
+//   var selectChar2 = symbolChar + numChar;
+// }
+// else if (passSymbolChar === true && passNumChar === false && passUpperChar === false && passLowerChar === false) {
+//   var selectChar3 = symbolChar;
+// }
+// else if (passSymbolChar === true && passNumChar === true && passUpperChar === false && passLowerChar === true) {
+//   var selectChar4 = symbolChar + numChar + lowerChar;
+// }
+// else if (passSymbolChar === true && passNumChar === false && passUpperChar === false && passLowerChar === true) {
+//   var selectChar5 = symbolChar + lowerChar;
+// }
+// else if (passSymbolChar === false && passNumChar === false && passUpperChar === false && passLowerChar === true) {
+//   var selectChar6 = lowerChar;
+// }
+// else if (passSymbolChar === false && passNumChar === true && passUpperChar === false && passLowerChar === false) {
+//   var selectChar7 = numChar;
+// }
+// else if (passSymbolChar === false && passNumChar === false && passUpperChar === true && passLowerChar === false) {
+//   var selectChar8 = upperChar;
+// }
+// else if (passSymbolChar === true && passNumChar === false && passUpperChar === true && passLowerChar === false) {
+//   var selectChar9 = symbolChar + upperChar;
+// }
+// else if (passSymbolChar === true && passNumChar === false && passUpperChar === true && passLowerChar === true) {
+//   var selectChar10 = symbolChar + upperChar + lowerChar;
+// }
+// else if (passSymbolChar === false && passNumChar === true && passUpperChar === true && passLowerChar === true) {
+//   var selectChar11 = numChar + upperChar + lowerChar;
+// }
+// else if (passSymbolChar === false && passNumChar === true && passUpperChar === true && passLowerChar === false) {
+//   var selectChar12 = numChar + upperChar;
+// }
+// else if (passSymbolChar === false && passNumChar === true && passUpperChar === false && passLowerChar === true) {
+//   var selectChar13 = numChar + lowerChar;
+// }
+// else if (passSymbolChar === false && passNumChar === false && passUpperChar === true && passLowerChar === true) {
+//   var selectChar14 = upperChar + lowerChar;
+// }
+
+// console.log(selectChar);
+
+//   for (var i = 0; i < passLength; i++) {
+//   var pass = Math.floor(Math.random() + passLength)
+
+// }  
 // if 
